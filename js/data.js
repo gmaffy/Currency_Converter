bf7b09054bf7a263657008957444bbaf15d3d9cb
Godwin Mafireyi <mafireyi@gmail.com>
2018-06-30 00:35:33 +0200

    changes

--- js/data.js
+++ js/data.js
@@ -1,7 +1,5 @@
 registeringSW();
-//loadCurrencies();
-loadCurFromDB();
-
+loadCurrencies();
 
 
 // Opening IDB
@@ -12,12 +10,16 @@ function openDatabase() {
     return idb.open('curr_converter1-db', 3, function (upgradeDb) {
         switch (upgradeDb.oldVersion) {
             case 0:
-                upgradeDb.createObjectStore('currency', {keyPath: "ABV"});
+                upgradeDb.createObjectStore('currency', {
+                    keyPath: "ABV"
+                });
             case 1:
-                upgradeDb.createObjectStore('conversions');
+                upgradeDb.createObjectStore('conversions', {
+                    keyPath: "FR_TO"
+                });
         }
     });
-};
+}
 
 
 //Registering SW
@@ -58,35 +60,34 @@ function registeringSW() {
 
 function updateReady(worker) {
     if (confirm("New version available")) {
-        worker.postMessage({ action: 'skipWaiting' });
+        worker.postMessage({
+            action: 'skipWaiting'
+        });
     } else {
-      return;
+        return;
     }
-}; 
+};
 
 // Track installing
 
 function trackInstalling(worker) {
-    const indexController = this;
+
     worker.addEventListener('statechange', function () {
         if (worker.state == 'installed') {
-            indexController._updateReady(worker);
+            updateReady(worker);
         }
     });
 };
 
-const selector = document.getElementsByTagName('select');
-
-console.log(selector);
-
 function loadCurrencies() {
+    const selector = document.getElementsByTagName('select');
     const dbPromise = openDatabase();
     dbPromise.then(function (db) {
         fetch("https://free.currencyconverterapi.com/api/v5/currencies")
             .then(function (response) {
+                console.log('response status', response.status)
                 if (response.status !== 200) {
                     console.warn('Sorry, there is a problem. Status code: ', response.status);
-                    return;
                 }
 
                 response.json().then(function (data) {
@@ -104,38 +105,45 @@ function loadCurrencies() {
                         selector[1].appendChild(opt2);
                         const tx = db.transaction("currency", "readwrite");
                         const curStore = tx.objectStore("currency");
-                        const cur = { ABV: currencies[curr].id, name: currencies[curr].currencyName }
+                        const cur = {
+                            ABV: currencies[curr].id,
+                            name: currencies[curr].currencyName
+                        }
                         curStore.put(cur);
                     }
 
                 })
 
-            });
+            }).catch(
+                loadCurFromDB()
+            );
 
     });
 }
 
 function loadCurFromDB() {
+    console.log('WORKING OFFLINE')
+    const selector = document.getElementsByTagName('select');
     const dbPromise = openDatabase();
     dbPromise.then((db) => {
         const tx = db.transaction("currency");
         const curStore = tx.objectStore("currency");
         return curStore.getAll();
-    }).then( (currency) => {
+    }).then((currency) => {
         for (cur of currency) {
-        console.log('kakaka',cur)
-        let opt = document.createElement("option");
-        let opt2 = document.createElement("option");
-        opt.value = cur['ABV'];
-        opt.text = cur['name'];
-        //console.log(currencies[curr].currencyName);
-        opt2.value = cur['ABV'];
-        opt2.text = cur['name'];
-        selector[0].appendChild(opt);
-        selector[1].appendChild(opt2);
+            //console.log('kakaka', cur)
+            let opt = document.createElement("option");
+            let opt2 = document.createElement("option");
+            opt.value = cur['ABV'];
+            opt.text = cur['name'];
+            //console.log(currencies[curr].currencyName);
+            opt2.value = cur['ABV'];
+            opt2.text = cur['name'];
+            selector[0].appendChild(opt);
+            selector[1].appendChild(opt2);
         }
     })
-   
+
 }
 
 function convertCur() {
@@ -148,48 +156,82 @@ function convertCur() {
         toCurVal.value = "";
         const fromTo = fromCur.value + "_" + toCur.value;
         const url = "https://free.currencyconverterapi.com/api/v5/convert?q=" + fromTo + "&compact=y";
-        fetch(url).then(response => {
-            response.json().then(data => {
-                console.log("DATA", data);
-                console.log("the value", data[fromTo].val);
-                const tx = db.transaction("conversions", "readwrite");
-                const convStore = tx.objectStore("conversions");
-                convStore.put(data[fromTo].val, fromTo);
-                toCurVal.value = parseFloat(fromCurVal.value) * data[fromTo].val;
-            });
-        });
+            fetch(url).then(response => {
+                if (response.status !== 200) {
+                    console.warn('Sorry, there is a problem. Status code: ', response.status);
+                }
+
+                response.json().then(data => {
+                    //console.log("DATA", data);
+                    //console.log("the value", data[fromTo].val);
+                    const tx = db.transaction("conversions", "readwrite");
+                    const convStore = tx.objectStore("conversions");
+                    const conv = {
+                        FR_TO: fromTo,
+                        VAL: data[fromTo].val
+                    }
+                    convStore.put(conv);
+                    toCurVal.value = parseFloat(fromCurVal.value) * data[fromTo].val;
+                });
+            }).catch(
+                convertFromDB()
+            );
+        
+
         //console.log('THIS IS THE AMT', fromCurVal.value);
         //console.log('THIS IS THE  FROM CURRENCY', fromCur.value);
         //console.log('THIS IS THE TO CURRENCY', toCur.value);
         //console.log('THIS IS THE CONVERTED CURRENCY', toCurVal.value);
 
-    })
-    
-};
+    });
 
-function btnClicked() {
-    const fromCurVal = document.getElementById("CURR_FR_VAL");
-    const fromCur = document.getElementById("CURR_FR");
-    const toCur = document.getElementById("CURR_TO")
-    let toCurVal = document.getElementById("CURR_VAL");
-    toCurVal.value = "";
-    const fromTo = fromCur.value + '_' + toCur.value;
-    const url = "https://free.currencyconverterapi.com/api/v5/convert?q=" + fromTo + "&compact=y";
-    fetch(url)
-        .then((response) => {
-            response.json().then((data) => {
-                console.log('DATA', data);
-                console.log('the value', data[fromTo].val);
-                toCurVal.value = parseFloat(fromCurVal.value) * data[fromTo].val;
-            })
-        })
-    //console.log('THIS IS THE AMT', fromCurVal.value);
-    //console.log('THIS IS THE  FROM CURRENCY', fromcur.value);
-    //console.log('THIS IS THE TO CURRENCY', toCur.value);
-    //console.log('THIS IS THE CONVERTED CURRENCY', toCurVal.value);
-};
+}
+
+
 
 function convertFromDB() {
+    console.log('working from offline database')
     const dbPromise = openDatabase();
-    
+    dbPromise.then((db) => {
+        
+        const tx = db.transaction("conversions");
+        const convStore = tx.objectStore("conversions");
+        return convStore.getAll();
+
+    }).then((conversions) => {
+        console.log(conversions.keys())
+        for (con of conversions) {
+            console.log('conves', con)
+            const fromCurVal = document.getElementById("CURR_FR_VAL");
+            const fromCur = document.getElementById("CURR_FR");
+            const toCur = document.getElementById("CURR_TO");
+            let toCurVal = document.getElementById("CURR_VAL");
+            toCurVal.value = "";
+            const fromTo = fromCur.value + "_" + toCur.value;
+            if (con[fromTo]) {
+                console.log('YIPPY')
+                toCurVal.value = parseFloat(fromCurVal.value) * con[fromTo]
+            }
+        }
+    })
+
+}
+
+function btnClicked() {
+    try {
+        convertCur();
+    } catch (error) {
+        console.log('Network lost, convertiing form db')
+        convertFromDB();
+    }
+
+}
+
+function checkAndLoad() {
+    try {
+        loadCurrencies();
+    } catch (error) {
+        loadCurFromDB();
+    }
+
 }
--- sw.js
+++ sw.js
@@ -6,6 +6,7 @@ self.addEventListener('install', function (event) {
                 '/',
                 'js/main.js',
                 'css/style.css',
+                'js/data.js'
             
             ]);
         })
@@ -26,56 +27,6 @@ self.addEventListener('activate', function (event) {
         })
     );
 });
-
-self.addEventListener('fetch', function (event) {
-    const requestUrl = new URL(event.request.url);
-
-    if (requestUrl.origin === location.origin) {
-        
-        if (requestUrl.pathname.startsWith('/photos/')) {
-            event.respondWith(servePhoto(event.request));
-            return;
-        }
-        // TODO: respond to avatar urls by responding with
-        // the return value of serveAvatar(event.request)
-    }
-
-    event.respondWith(
-        caches.match(event.request).then(function (response) {
-            return response || fetch(event.request);
-        })
-    );
-});
-
-function serveAvatar(request) {
-    // Avatar urls look like:
-    // avatars/sam-2x.jpg
-    // But storageUrl has the -2x.jpg bit missing.
-    // Use this url to store & match the image in the cache.
-    // This means you only store one copy of each avatar.
-    var storageUrl = request.url.replace(/-\dx\.jpg$/, '');
-
-    // TODO: return images from the "wittr-content-imgs" cache
-    // if they're in there. But afterwards, go to the network
-    // to update the entry in the cache.
-    //
-    // Note that this is slightly different to servePhoto!
-}
-
-function servePhoto(request) {
-    var storageUrl = request.url.replace(/-\d+px\.jpg$/, '');
-
-    return caches.open(contentImgsCache).then(function (cache) {
-        return cache.match(storageUrl).then(function (response) {
-            if (response) return response;
-
-            return fetch(request).then(function (networkResponse) {
-                cache.put(storageUrl, networkResponse.clone());
-                return networkResponse;
-            });
-        });
-    });
-}
 
 self.addEventListener('message', function (event) {
     if (event.data.action === 'skipWaiting') {
